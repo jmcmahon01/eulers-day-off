9e8c62f3cc54133103567c626488e106ab0f81c0
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-12-14 10:33:53 -0800

    run test for getArray and findThreeMultiples

--- __tests__/euler.test.js
+++ __tests__/euler.test.js
@@ -1,10 +1,20 @@
-import findMultiples from "./../src/euler.js";
-describe("findMultiples", ()=> {
-  test('should return an array of every multiple of 3 in the number', ()=> {
-    const number = 10;
-    expect(findMultiples(number)).toEqual([3, 6, 9]);
-  });
+import { getArray, findThreeMultiples } from "./../src/euler.js";
+
+describe('getArray', () => {
+    test('should return an array of all natural numbers ranging from 1 to the number in parameter', () => {
+        const number = 5;
+        expect(getArray(number)).toEqual([1, 2, 3, 4, 5]);
+    });
 });
+
+describe("findThreeMultiples", ()=> {
+    test('should return an array of every multiple of 3 in the number', ()=> {
+      const number = 10;
+      expect(findThreeMultiples(number)).toEqual([3, 6, 9]);
+    });
+});
+
+
 
 /*
 import formatName from '../src/name.js';
--- src/euler.js
+++ src/euler.js
@@ -1,17 +1,25 @@
-export default function findMultiples(num) {
-    let returnArray=[];
-    let multipleThreeArr = [];
-    for (let i=1; i<=num; i++){
-        returnArray.push(i);
-    }
+export { getArray, findThreeMultiples };
+
+function getArray(num) {
+    let array=[];
+    for (let i = 1; i <= num; i++){
+        array.push(i);
+    };
+    return array;
+};
+
+function findThreeMultiples(num) {
+    const array = getArray(num);
+    let threeArr = [];
     // loop through returnArray, using modulo, see if the return value is 0
-    for (let j = 0; j <= returnArray.length; j++) {
-        if (returnArray[j] % 3 === 0) {
-            multipleThreeArr.push(returnArray[j]);
+    for (let j = 0; j <= array.length; j++) {
+        if (array[j] % 3 === 0) {
+            threeArr.push(array[j]);
         }
     }
-    return multipleThreeArr;
+    return threeArr;
 }
 
 // var % 3 = 0
 // if more than 0 = not divisible by 3.
+
