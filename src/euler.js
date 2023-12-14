47bf355d6b39937822dbf5c717d286ac2614d238
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-12-14 10:49:24 -0800

    run test for findFiveMultiples, test passes

--- src/euler.js
+++ src/euler.js
@@ -1,26 +1,34 @@
 export { getArray, findThreeMultiples, findFiveMultiples };
 
 function getArray(num) {
-    let array=[];
-    for (let i = 1; i <= num; i++){
-        array.push(i);
-    };
-    return array;
+  let array = [];
+  for (let i = 1; i <= num; i++) {
+    array.push(i);
+  };
+  return array;
 };
 
 function findThreeMultiples(num) {
-    const array = getArray(num);
-    let threeArr = [];
-    // loop through returnArray, using modulo, see if the return value is 0
-    for (let j = 0; j <= array.length; j++) {
-        if (array[j] % 3 === 0) {
-            threeArr.push(array[j]);
-        }
+  const array = getArray(num);
+  let threeArr = [];
+  // loop through returnArray, using modulo, see if the return value is 0
+  for (let j = 0; j <= array.length; j++) {
+    if (array[j] % 3 === 0) {
+      threeArr.push(array[j]);
     }
-    return threeArr;
+  }
+  return threeArr;
 }
-function findFiveMultiples() {
-    
+function findFiveMultiples(num) {
+  const array = getArray(num);
+  let fiveArr = [];
+  // loop through returnArray, using modulo, see if the retur value is 0
+  for (let k = 0; k <= array.length; k++) {
+    if (array[k] % 5 === 0) {
+      fiveArr.push(array[k]);
+    }
+  }
+  return fiveArr;
 }
 // var % 3 = 0
 // if more than 0 = not divisible by 3.
